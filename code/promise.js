b4c34786300e41b276001341d6edecfcf39d061f
AlanWen2016 <906075319@qq.com>
2019-09-22 19:02:30 +0800

    增加promise

---
+++ code/promise.js
@@ -0,0 +1,15 @@
+const PENDDING = 'PENDING';
+CONST FULLFILLED = 'FULLFILLED';
+CONST 
+class Promise {
+    constructor(executor){
+        
+        let reslove = ()=>{
+
+        }
+        let reject = ()=>{
+
+        }
+    }
+
+}
